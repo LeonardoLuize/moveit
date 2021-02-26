a2593c83d77db48271df9ccd27cba3a7f1898adb
Leonardo Luize <74014082+LeonardoLuize@users.noreply.github.com>
2021-02-26 14:05:16 -0300

    moveit_nlw

---
+++ src/components/Countdown.tsx
@@ -0,0 +1,71 @@
+
+import { useContext, useEffect, useState } from 'react';
+import { ChallengeContext } from '../context/ChallengeContext';
+import { CountdownContext } from '../context/CountdownsContext';
+import styles from '../styles/components/Countdown.module.css';
+
+export function Countdown(){
+
+    const { 
+        minutes, 
+        seconds, 
+        hasFinished, 
+        isActive,
+        startCountdown,
+        resetCountdown 
+    } = useContext(CountdownContext);
+
+    const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+    const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+
+   
+
+    return(
+        <div>
+            <div className={styles.countdownContainer}>
+                <div>
+                    <span> {minuteLeft} </span>
+                    <span> {minuteRight} </span>
+                </div>
+                <span>:</span>
+                <div>
+                    <span> {secondLeft} </span>
+                    <span> {secondRight} </span>  
+                </div>
+            </div>  
+
+            {hasFinished ? (
+                <button disabled onClick={resetCountdown} type="button" className={styles.countdownButton}>
+                    Ciclo encerrado <img src="icons/check.svg" alt="Check"/>
+                </button>
+            ):(
+                <>
+                
+                {isActive ? (
+
+                <button onClick={resetCountdown} type="button" className={`${styles.countdownButton} ${styles.countdownButtonActive}`}>
+                    Abandonar ciclo
+
+                    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M14 1.41L12.59 0L7 5.59L1.41 0L0 1.41L5.59 7L0 12.59L1.41 14L7 8.41L12.59 14L14 12.59L8.41 7L14 1.41Z" fill="white"/>
+                    </svg>
+                </button>
+
+                ):(
+
+                <button onClick={startCountdown} type="button" className={styles.countdownButton}>
+                    Iniciar um ciclo
+                </button>
+
+                )}
+                
+                </>
+            )}
+
+            
+            
+
+            
+        </div>
+    );
+}
